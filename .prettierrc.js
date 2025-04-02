d2b143a90a10341be5060fc7531549186485202a
Tej Powar <tej@powtech.co.uk>
2025-04-02 14:20:07 +0100

    remove unwanted comments from the prettierrc file

--- .prettierrc.js
+++ .prettierrc.js
@@ -8,21 +8,10 @@ export default {
   singleQuote: true,
   trailingComma: 'none',
   overrides: [
-    // Keep any other overrides you might already have
-    // ...
-
-    // Add this object to handle Nunjucks files
     {
-      // Files to target: typical Nunjucks extensions
       files: ['*.njk', '*.nunjucks'],
-      // Options specific to these files
       options: {
-        // Specify the parser provided by the plugin
         parser: 'jinja-template'
-        // You can add other Nunjucks-specific options here if needed,
-        // but usually, just setting the parser is enough.
-        // For example, if you wanted a different printWidth ONLY for njk files:
-        // printWidth: 120,
       }
     }
   ]
