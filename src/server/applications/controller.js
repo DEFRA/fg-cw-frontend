644a13d6d4c85cb954e0cbfde445fd7c316c0661
julian <juliankimmings@gmail.com>
2025-04-11 16:34:18 +0100

    Add tags and colour variants
    - to enable viewing of various tags add more data
    - set status as the content for tag
    - capitalise status as per design system

--- src/server/applications/controller.js
+++ src/server/applications/controller.js
@@ -13,7 +13,7 @@ export const applicationsData = {
       caseType: 'Water management R3',
       caseName: 'Northampton Reservoir',
       businessName: 'Farming Group Ltd',
-      status: 'NEW',
+      status: 'IN PROGRESS',
       dateReceived: '2025-03-27T11:34:52Z',
       targetDate: '2025-04-27T11:34:52Z',
       priority: 'MEDIUM',
@@ -71,6 +71,120 @@ export const applicationsData = {
       caseName: 'Yorkshire Reservoir',
       businessName: 'Hens Ltd',
       status: 'NEW',
+      dateReceived: '2025-03-27T11:34:52Z',
+      targetDate: '2025-04-27T11:34:52Z',
+      priority: 'MEDIUM',
+      assignedUser: 'Tej Powar',
+      actionGroups: [
+        {
+          id: '1',
+          actions: [
+            {
+              id: '1',
+              tasks: [
+                {
+                  id: '1',
+                  value: 'YES'
+                },
+                {
+                  id: '2',
+                  value: 'YES'
+                }
+              ],
+              status: 'COMPLETED'
+            },
+            {
+              id: '2',
+              tasks: [
+                {
+                  id: '1',
+                  value: null
+                }
+              ],
+              status: 'NOT STARTED'
+            }
+          ]
+        },
+        {
+          id: '2',
+          actions: {
+            id: '1',
+            tasks: [
+              {
+                id: '1',
+                value: null
+              }
+            ],
+            status: 'CANNOT START YET'
+          }
+        }
+      ]
+    },
+    {
+      id: '100003',
+      workflowId: '124',
+      caseRef: 'CASE-REF-2',
+      caseType: 'Water management R4',
+      caseName: 'Yorkshire Reservoir',
+      businessName: 'Hens Ltd',
+      status: 'APPROVED',
+      dateReceived: '2025-03-27T11:34:52Z',
+      targetDate: '2025-04-27T11:34:52Z',
+      priority: 'MEDIUM',
+      assignedUser: 'Tej Powar',
+      actionGroups: [
+        {
+          id: '1',
+          actions: [
+            {
+              id: '1',
+              tasks: [
+                {
+                  id: '1',
+                  value: 'YES'
+                },
+                {
+                  id: '2',
+                  value: 'YES'
+                }
+              ],
+              status: 'COMPLETED'
+            },
+            {
+              id: '2',
+              tasks: [
+                {
+                  id: '1',
+                  value: null
+                }
+              ],
+              status: 'NOT STARTED'
+            }
+          ]
+        },
+        {
+          id: '2',
+          actions: {
+            id: '1',
+            tasks: [
+              {
+                id: '1',
+                value: null
+              }
+            ],
+            status: 'CANNOT START YET'
+          }
+        }
+      ]
+    },
+    {
+      id: '100004',
+      workflowId: '124',
+      caseRef: 'CASE-REF-2',
+      caseType: 'Water management R4',
+      caseName: 'Yorkshire Reservoir',
+      businessName: 'Hens Ltd',
+      status: 'NOT STARTED',
       dateReceived: '2025-03-27T11:34:52Z',
       targetDate: '2025-04-27T11:34:52Z',
       priority: 'MEDIUM',
