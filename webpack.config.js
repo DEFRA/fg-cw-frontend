a77bc1454aca86109abb239050e85ecdfd7016d1
I A Nawaz <ianawaz@Is-MacBook-Pro.local>
2025-04-17 13:55:00 +0100

    Removing unnecessary comments

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ export default {
     application: {
       import: [
         './javascripts/application.js',
-        './javascripts/modules/checkbox-select-all.js', // ✅ Added here
+        './javascripts/modules/checkbox-select-all.js', 
         './stylesheets/application.scss'
       ]
     },
