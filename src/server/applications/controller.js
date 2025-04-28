86f8d3e2681830cfce6cdd5d8f36bd7138e93952
Dev-Islas <islas104@gmail.com>
2025-04-28 10:56:33 +0100

    FUT-318: CW-FE - Show Selected Case - Display Case Details in Case Management System (#14)
    * Add initial route and controller for case detail page (FUT-318)
    
    * Link application refs to details page and display case summary
    
    * Create views directory. Add show case view.
    
    * refactor: update applicant view to match GOV.UK design
    
    * Update application view to match Defra standards with dynamic heading, submitted date and section layout
    
    * remove commented json. update date format.
    
     - and loop through data and template what we’re given in the json BLOB
    
    ---------
    
    Co-authored-by: julian <juliankimmings@gmail.com>

--- src/server/applications/controller.js
+++ src/server/applications/controller.js
@@ -18,24 +18,40 @@ const getCases = async () => {
   }
 }
 
+const getCaseById = async (caseId) => {
+  try {
+    const backendUrl = config.get('fg_cw_backend_url')
+    const response = await fetch(`${backendUrl.toString()}/cases/${caseId}`)
+    const data = await response.json()
+    return data
+  } catch (error) {
+    return null
+  }
+}
+
 export const applicationsController = {
   handler: async (_request, h) => {
     const caseData = await getCases()
-    return h.view('applications/index', {
+    return h.view('applications/views/index', {
       pageTitle: 'Applications',
       heading: 'Applications',
-      breadcrumbs: [
-        {
-          text: 'Home',
-          href: '/'
-        },
-        {
-          text: 'Applications'
-        }
-      ],
-      data: {
-        allCases: caseData
-      }
+      breadcrumbs: [{ text: 'Home', href: '/' }, { text: 'Applications' }],
+      data: { allCases: caseData }
+    })
+  },
+
+  show: async (request, h) => {
+    const caseId = request.params.id
+    const selectedCase = await getCaseById(caseId)
+
+    if (!selectedCase) {
+      return h.response('Case not found').code(404)
+    }
+
+    return h.view('applications/views/show', {
+      pageTitle: 'Case Detail',
+      heading: selectedCase.businessName || 'Case Detail',
+      caseData: selectedCase
     })
   }
 }
