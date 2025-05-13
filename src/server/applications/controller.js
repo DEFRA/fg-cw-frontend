fecd972f2dec3894a009c0bbe039d8e0da5be804
julian <juliankimmings@gmail.com>
2025-05-13 14:57:12 +0100

    update post

--- src/server/applications/controller.js
+++ src/server/applications/controller.js
@@ -36,7 +36,7 @@ const updateStageAsync = async (caseId, nextStage) => {
       `${backendUrl.toString()}/cases/${caseId}/stage`,
       {
         method: 'POST',
-        data: {
+        body: {
           nextStage
         }
       }
@@ -178,8 +178,8 @@ export const applicationsController = {
 
     const { nextStage } = request.payload
 
-    await updateStageAsync(id, nextStage)
     // call backend with stage update
+    await updateStageAsync(id, nextStage)
     // redirect to stage
     return showApplication(request, h)
   },
