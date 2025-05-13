cb5faea3ce753b8f16d825acd82e879da67f2075
Aditya Kolnati <aditya.kolnati@gmail.com>
2025-05-13 06:58:51 +0100

    implement stage, task group and list view (#22)
    * implement stage, task group and list view
    
    * remove mock stage section
    
    ---------
    
    Co-authored-by: Aditya Kolnati <adityakolnati@adityas.mac.pro.m2.max.chip.lan>

---
+++ src/server/common/components/taskGroups/taskGroupsTemplate.test.js
@@ -0,0 +1,67 @@
+import { renderComponent } from '../../test-helpers/component-helpers.js'
+
+describe('Task Groups Component', () => {
+  /** @type {CheerioAPI} */
+  let $taskGroups
+
+  const stages = [
+    {
+      title: 'Application',
+      groups: [
+        {
+          title: 'Check application',
+          tasks: [
+            {
+              title: 'Check application and documents',
+              link: '/cases/APPLICATION-REF-1/task-group/1',
+              status: 'COMPLETED'
+            },
+            {
+              title: 'Check for dual funding',
+              link: '/cases/APPLICATION-REF-1/task-group/2',
+              status: 'NOT STARTED'
+            }
+          ]
+        },
+        {
+          title: 'Make Application Decision',
+          tasks: [
+            {
+              title: 'Approve or reject application',
+              link: '/cases/APPLICATION-REF-1/task-group/1',
+              status: 'NOT STARTED'
+            }
+          ]
+        }
+      ]
+    }
+  ]
+
+  beforeEach(() => {
+    $taskGroups = renderComponent('taskGroups', {
+      stages: stages
+    })
+  })
+
+  test('Should not render taskGroups component when no stages', () => {
+    $taskGroups = renderComponent('taskGroups', { stages: [] })
+    expect($taskGroups('[data-testid="stage-heading"]')).toHaveLength(0)
+  })
+
+  test('Should render stage headings', () => {
+    expect($taskGroups('[data-testid="stage-heading"]')).toHaveLength(1)
+    expect($taskGroups('[data-testid="stage-heading"]').text().trim()).toBe(
+      'Application'
+    )
+  })
+
+  test('Should render task content', () => {
+    const text = $taskGroups('body').text()
+    expect(text).toContain('Check application')
+    expect(text).toContain('Make Application Decision')
+    expect(text).toContain('Check for dual funding')
+  })
+})
+/**
+ * @import { CheerioAPI } from 'cheerio'
+ */
