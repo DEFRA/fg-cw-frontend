529c074e22199e9951aaf73af4efb94eec1720e0
Aditya Kolnati <adityakolnati@adityas.mac.pro.m2.max.chip.lan>
2025-06-06 08:06:37 +0100

    code coverage set to 85 percetage

--- vitest.config.js
+++ vitest.config.js
@@ -50,10 +50,10 @@ export default defineConfig({
       extension: ['.js', '.ts'], // Failed to load coverage without ts extension
       include: ['src/**/*.{js,ts}'], // Failed to load coverage without ts extension
       thresholds: {
-        statements: 50,
-        branches: 50,
-        functions: 50,
-        lines: 50
+        statements: 85,
+        branches: 85,
+        functions: 85,
+        lines: 85
       },
       reportOnFailure: true,
       ignoreEmptyLines: false,
