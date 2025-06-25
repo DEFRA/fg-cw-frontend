f7cfdaaf0d93707f7038a1c280010d5a386c1e59
Julian Kimmings <35488092+kimmiju@users.noreply.github.com>
2025-06-25 14:45:39 +0100

    add proxyAgent for http(s) requests (#83)

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import process from "node:process";
 import { logger } from "./common/logger.js";
 import { createServer } from "./server.js";
+import { ProxyAgent } from "proxy-agent";
+import http from "http";
+import https from "https";
+
+http.globalAgent = new ProxyAgent();
+https.globalAgent = new ProxyAgent();
 
 process.on("unhandledRejection", (error) => {
   logger.error(error, "Unhandled rejection");
