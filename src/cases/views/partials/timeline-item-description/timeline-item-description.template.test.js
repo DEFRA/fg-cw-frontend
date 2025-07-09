dcac51bf67d01832ba0319dd7c9f00b4aceca31e
Julian Kimmings <35488092+kimmiju@users.noreply.github.com>
2025-07-09 11:03:00 +0100

    FUT-523 - Adds timeline ui (#93)
    * Adds timeline ui
    
    * update sass
    
    * update snapshots
    
    * fix date
    
    * set timezone for data formatting

---
+++ src/cases/views/partials/timeline-item-description/timeline-item-description.template.test.js
@@ -0,0 +1,23 @@
+import { describe, expect, it } from "vitest";
+import { render } from "../../../../common/nunjucks/render.js";
+
+describe("timeline-item-description", () => {
+  it("renders case assigned view", () => {
+    const component = render("timeline-item-description", {
+      eventType: "CASE_ASSIGNED",
+      data: {
+        assignedTo: "Mickey Mouse",
+        assignedBy: "Donald Duck",
+      },
+    });
+    expect(component).toMatchSnapshot();
+  });
+
+  it("renders other view", () => {
+    const component = render("timeline-item-description", {
+      eventType: "NOTE_ADDED",
+    });
+
+    expect(component).toMatchSnapshot();
+  });
+});
--- src/common/nunjucks/filters/format-date.js
+++ src/common/nunjucks/filters/format-date.js
@@ -1,7 +1,8 @@
+import { tz } from "@date-fns/tz";
 import { format, isDate, parseISO } from "date-fns";
 
 export const formatDate = (value, formattedDateStr = "EEE do MMMM yyyy") => {
   const date = isDate(value) ? value : parseISO(value);
 
-  return format(date, formattedDateStr);
+  return format(date, formattedDateStr, { in: tz("Europe/London") });
 };
