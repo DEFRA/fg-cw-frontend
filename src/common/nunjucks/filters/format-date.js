5da625d58b4b80717ef75aef52f9df63e8ac31f3
Donatas Bacius <bacius@gmail.com>
2025-07-09 14:06:15 +0100

    Use same time zone across the app

--- src/common/nunjucks/filters/format-date.js
+++ src/common/nunjucks/filters/format-date.js
@@ -1,8 +1,7 @@
-import { tz } from "@date-fns/tz";
 import { format, isDate, parseISO } from "date-fns";
 
 export const formatDate = (value, formattedDateStr = "EEE do MMMM yyyy") => {
   const date = isDate(value) ? value : parseISO(value);
 
-  return format(date, formattedDateStr, { in: tz("Europe/London") });
+  return format(date, formattedDateStr);
 };
