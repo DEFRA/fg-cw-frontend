358c8d22c126a0d84b35123318830827a8152625
Justin Ramel <justin.ramel@gmail.com>
2025-08-07 15:11:10 +0100

    add route tests for adding a new note

--- src/cases/routes/notes/new-note.route.test.js
+++ src/cases/routes/notes/new-note.route.test.js
@@ -3,16 +3,16 @@ import { load } from "cheerio";
 import { afterAll, beforeAll, describe, expect, it, vi } from "vitest";
 import { nunjucks } from "../../../common/nunjucks/nunjucks.js";
 import { findCaseByIdUseCase } from "../../use-cases/find-case-by-id.use-case.js";
-import { viewNotesRoute } from "./view-notes.route.js";
+import { newNoteRoute } from "./new-note.route.js";
 
 vi.mock("../../use-cases/find-case-by-id.use-case.js");
 
-describe("viewNotesRoute", () => {
+describe("newNoteRoute", () => {
   let server;
 
   beforeAll(async () => {
     server = hapi.server();
-    server.route(viewNotesRoute);
+    server.route(newNoteRoute);
     await server.register([nunjucks]);
 
     await server.initialize();
@@ -29,7 +29,7 @@ describe("viewNotesRoute", () => {
 
     await server.inject({
       method: "GET",
-      url: "/cases/68495db5afe2d27b09b2ee47/notes",
+      url: "/cases/68495db5afe2d27b09b2ee47/notes/new",
     });
 
     expect(findCaseByIdUseCase).toHaveBeenCalledWith(
@@ -44,47 +44,7 @@ describe("viewNotesRoute", () => {
 
     const { statusCode, result } = await server.inject({
       method: "GET",
-      url: "/cases/68495db5afe2d27b09b2ee47/notes",
-    });
-
-    expect(statusCode).toEqual(200);
-
-    const $ = load(result);
-    const view = $("#main-content").html();
-
-    expect(view).toMatchSnapshot();
-  });
-
-  it("handles case with no comments", async () => {
-    const mockCaseData = createMockCaseData({
-      comments: [],
-    });
-
-    findCaseByIdUseCase.mockResolvedValue(mockCaseData);
-
-    const { statusCode, result } = await server.inject({
-      method: "GET",
-      url: "/cases/68495db5afe2d27b09b2ee47/notes",
-    });
-
-    expect(statusCode).toEqual(200);
-
-    const $ = load(result);
-    const view = $("#main-content").html();
-
-    expect(view).toMatchSnapshot();
-  });
-
-  it("handles case with null comments", async () => {
-    const mockCaseData = createMockCaseData({
-      comments: null,
-    });
-
-    findCaseByIdUseCase.mockResolvedValue(mockCaseData);
-
-    const { statusCode, result } = await server.inject({
-      method: "GET",
-      url: "/cases/68495db5afe2d27b09b2ee47/notes",
+      url: "/cases/68495db5afe2d27b09b2ee47/notes/new",
     });
 
     expect(statusCode).toEqual(200);
