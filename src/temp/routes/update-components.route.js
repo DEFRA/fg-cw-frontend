d6a0bf6cf51a92936257bececbeef29f8f74989c
Justin Ramel <justin.ramel@gmail.com>
2025-10-13 11:47:54 +0100

    Temp component render (#167)
    * add temp routes to dynamically render components
    
    * add link to docs
    
    * Refactor dynamic components routes and handlers.
    
    * add auth context to routes
    
    * Add unit tests for dynamic components use cases, view models, and routes

---
+++ src/temp/routes/update-components.route.js
@@ -0,0 +1,35 @@
+import { findCaseByIdUseCase } from "../../cases/use-cases/find-case-by-id.use-case.js";
+import { updateComponentsPreviewUseCase } from "../use-cases/components.use-case.js";
+import { createComponentsEditViewModel } from "../view-models/components.view-model.js";
+
+export const updateComponentsRoute = {
+  method: "POST",
+  path: "/cases/{caseId}/components/edit",
+  handler: async (request, h) => {
+    const { caseId } = request.params;
+
+    const authContext = {
+      token: request.auth.credentials.token,
+      user: request.auth.credentials.user,
+    };
+
+    const caseItem = await findCaseByIdUseCase(authContext, caseId);
+    const jsonPayload = request.payload?.jsonPayload;
+    const formData = { jsonPayload };
+    const result = updateComponentsPreviewUseCase({
+      session: request.yar,
+      jsonPayload,
+    });
+
+    if (result.errors) {
+      const viewModel = createComponentsEditViewModel(caseItem, {
+        formData,
+        errors: result.errors,
+      });
+
+      return h.view("temp/components-edit", viewModel);
+    }
+
+    return h.redirect(`/cases/${caseId}/components`);
+  },
+};
