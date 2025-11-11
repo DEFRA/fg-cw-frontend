c08bdb40757f5cbac2340e0c5b58e51daab14b8f
Justin Ramel <justin.ramel@esynergy.co.uk>
2025-11-11 15:14:21 +0000

    Remove double init of gov components (#212)

--- src/client/javascripts/application.js
+++ src/client/javascripts/application.js
@@ -1,26 +1,7 @@
-import {
-  Accordion,
-  Button,
-  Checkboxes,
-  createAll,
-  ErrorSummary,
-  Header,
-  Radios,
-  SkipLink,
-  Tabs,
-} from "govuk-frontend";
+import { createAll } from "govuk-frontend";
 
 import { CopyToClipboard } from "../../common/components/copy-to-clipboard/copy-to-clipboard.js";
 import { ExpandableText } from "../../common/components/expandable-text/expandable-text.js";
-
-createAll(Accordion);
-createAll(Button);
-createAll(Checkboxes);
-createAll(ErrorSummary);
-createAll(Header);
-createAll(Radios);
-createAll(SkipLink);
-createAll(Tabs);
 
 createAll(ExpandableText);
 createAll(CopyToClipboard);
--- src/client/javascripts/application.test.js
+++ src/client/javascripts/application.test.js
@@ -1,14 +1,4 @@
-import {
-  Accordion,
-  Button,
-  Checkboxes,
-  createAll,
-  ErrorSummary,
-  Header,
-  Radios,
-  SkipLink,
-  Tabs,
-} from "govuk-frontend";
+import { createAll } from "govuk-frontend";
 import { afterEach, describe, expect, it, vi } from "vitest";
 import { CopyToClipboard } from "../../common/components/copy-to-clipboard/copy-to-clipboard.js";
 import { ExpandableText } from "../../common/components/expandable-text/expandable-text.js";
@@ -23,29 +13,8 @@ describe("application.js", () => {
   it("should import all required GOV.UK Frontend components", async () => {
     await import("./application.js");
 
-    expect(createAll).toHaveBeenCalledTimes(10);
-    expect(createAll).toHaveBeenCalledWith(Accordion);
-    expect(createAll).toHaveBeenCalledWith(Button);
-    expect(createAll).toHaveBeenCalledWith(Checkboxes);
-    expect(createAll).toHaveBeenCalledWith(ErrorSummary);
-    expect(createAll).toHaveBeenCalledWith(Header);
-    expect(createAll).toHaveBeenCalledWith(Radios);
-    expect(createAll).toHaveBeenCalledWith(SkipLink);
-    expect(createAll).toHaveBeenCalledWith(Tabs);
+    expect(createAll).toHaveBeenCalledTimes(2);
     expect(createAll).toHaveBeenCalledWith(ExpandableText);
     expect(createAll).toHaveBeenCalledWith(CopyToClipboard);
-  });
-
-  it("should initialize components in the correct order", async () => {
-    await import("./application.js");
-
-    expect(createAll).toHaveBeenNthCalledWith(1, Accordion);
-    expect(createAll).toHaveBeenNthCalledWith(2, Button);
-    expect(createAll).toHaveBeenNthCalledWith(3, Checkboxes);
-    expect(createAll).toHaveBeenNthCalledWith(4, ErrorSummary);
-    expect(createAll).toHaveBeenNthCalledWith(5, Header);
-    expect(createAll).toHaveBeenNthCalledWith(6, Radios);
-    expect(createAll).toHaveBeenNthCalledWith(7, SkipLink);
-    expect(createAll).toHaveBeenNthCalledWith(8, Tabs);
   });
 });
