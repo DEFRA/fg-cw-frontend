80c7f52b1f34e19e0ea816bccfd26f80e9cc8ce5
Justin Ramel <justin.ramel@esynergy.co.uk>
2026-01-15 11:42:40 +0000

    Handle null/undefined dates in formatDate function and update related tests

--- src/common/nunjucks/filters/format-date.js
+++ src/common/nunjucks/filters/format-date.js
@@ -1,10 +1,15 @@
 import { format, isDate, parseISO } from "date-fns";
+import { isNil } from "lodash";
 
 export const DATE_FORMAT_SHORT_MONTH = "d MMM yyyy";
+export const DATE_FORMAT_SHORT_DATE_TIME = `${DATE_FORMAT_SHORT_MONTH} HH:mm`;
 export const DATE_FORMAT_SORTABLE_DATE = "yyyy-MM-dd";
 
 export const formatDate = (value, formattedDateStr = "EEE do MMMM yyyy") => {
-  const date = isDate(value) ? value : parseISO(value);
+  if (isNil(value)) {
+    return "";
+  }
 
+  const date = isDate(value) ? value : parseISO(value);
   return format(date, formattedDateStr);
 };
